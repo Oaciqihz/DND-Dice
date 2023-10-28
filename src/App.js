c734713f2464bf47f367a6af322034e39902b296
2391580279@qq.com <2391580279@qq.com>
2023-10-29 01:08:43 +0800

    en: 多类型骰子添加

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Box, Button, Input, List, Select, Text } from '@chakra-ui/react';
 import './App.css';
 import "./style/App.scss";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getRandomInt } from './utils/string/getRandomInt';
 import { CusotmIcon } from './components/CutsomIcon';
 import { getTimeStamp } from './utils/date';
@@ -18,14 +18,29 @@ function App() {
   ]
   
   let [roleAttr, setRoleAttr] = useState([]);
-  let [diceObj, setDiceObj] = useState({
+  let [diceObj, setDiceObj] = useState([{
     num: 1, side: 6,
-  });
+  }]);
   let [rollLog, setRollLog] = useState([]);
 
-  function changeDiceObj(key, value) {
-      diceObj[key] = value;
-      setDiceObj({...diceObj});
+  // 设置骰子属性
+  function changeDiceObj(key, value, index) {
+    diceObj[index][key] = value;
+    setDiceObj([...diceObj]);
+  }
+
+  // 添加骰子
+  function addDice() {
+    diceObj.push({
+      num: 1, side: 6
+    })
+    setDiceObj([...diceObj]);
+  }
+
+  // 移除骰子
+  function delDice(index) {
+    diceObj.splice(index, 1);
+    setDiceObj([...diceObj]);
   }
 
   // 添加角色属性
@@ -51,15 +66,19 @@ function App() {
 
   // 掷骰子
   function rollDice() {
-    const { num, side } = diceObj;
     let total = 0;
     let log = "";
-    // 获取骰子随机数
-    for (let i = 0; i < num; i++) {
-      const randomNum = getRandomInt(1, side);
-      total += randomNum;
-      log = log + randomNum + ","
-    }
+    let rollLine = "";
+    diceObj.forEach(dice => {
+      const { num, side } = dice;
+      rollLine = rollLine + `${num}d${side}, `
+      // 获取骰子随机数
+      for (let i = 0; i < num; i++) {
+        const randomNum = getRandomInt(1, side);
+        total += randomNum;
+        log = log + randomNum + ","
+      }
+    })
     // 总和 + 属性
     roleAttr.forEach(attr => {
       if (attr.type === "add") {
@@ -73,23 +92,34 @@ function App() {
     // 添加到历史记录中
     rollLog.push({
       time: getTimeStamp(),
-      rollLine: `${num}d${side}`,
+      rollLine: rollLine,
       total: `${log}`,
       totalNum: total
     });
     setRollLog([...rollLog]);
   }
 
+  // 下拉Log栏
+  function scrollLog() {
+    const dom = document.querySelector(".List");
+    dom.scrollTop = dom.scrollHeight;
+  }
+
+  // 监听rollLog更新下拉Log栏
+  useEffect(() => {
+    rollLog && rollLog.length !== 0 && scrollLog();
+  },[rollLog])
+
   return (
     <div className="App">
 
+      {/* 角色设置 */}
       <Text 
         fontSize={30} 
         fontWeight={600} 
         fontFamily="Times New Roman" 
         textShadow="1px 2px #fff"
       >ROLE ATTRIBUTES</Text>
-
       {
         roleAttr.map((attr, index) => 
           <div className="role" key={index}>
@@ -116,6 +146,8 @@ function App() {
       }
       <Button mt={5} onClick={addRoleAttr}>ADD ROLE ATTRIBUTES</Button>
 
+
+      {/* 骰子设置 */}
       <Text 
         mt="30px"
         fontSize={30} 
@@ -124,7 +156,51 @@ function App() {
         textShadow="1px 2px #fff"
       >CHOOSE YOUR DICE</Text>
       {
-        rollLog.length !== 0 &&
+        diceObj.map((dice, index) => 
+          <div className="choose-dice" key={index}>
+            {/* 骰子个数 */}
+            <Input 
+              height={30}
+              min={1}
+              type="number" 
+              value={dice.num} 
+              onChange={(e) => {
+                const inputValue = e.target.value;
+                const newValue = inputValue !== "" ? Number(inputValue) : "";
+                changeDiceObj("num", newValue, index);
+              }} 
+            />
+            {/* 骰子面数 */}
+            <Select 
+              height={30}
+              value={dice.side} 
+              onChange={(e) => changeDiceObj("side", Number(e.target.value), index)}
+            >
+              {
+                diceSideOptions.map(e => 
+                  <option value={e.value} key={e.value}>{e.label}</option>
+                )
+              }
+            </Select>
+            {/* 移除骰子 */}
+            {
+              diceObj.length !== 1 &&
+              <Button className="btn-reduce" onClick={() => delDice(index)}>
+                {CusotmIcon({key: "reduce"})}
+              </Button>
+            }
+          </div>
+        )
+      }
+      <Box display="flex" flexDirection="column" alignItems="center" mt="20px">
+        {/* 添加骰子 */}
+        <Button 
+          minW="30px" 
+          onClick={addDice}
+        >ADD DICE</Button>
+        {/* 当前骰子点数 */}
+        {
+          rollLog.length !== 0 &&
           <Text 
             color="#a52714"
             fontSize={25} 
@@ -134,53 +210,50 @@ function App() {
           >
               {rollLog[rollLog.length - 1].totalNum}
           </Text>
-      }
-      <div className="choose-dice">
-        {/* 骰子个数 */}
-        <Input 
-          min={1}
-          type="number" 
-          value={diceObj.num} 
-          onChange={(e) => {
-            const inputValue = e.target.value;
-            const newValue = inputValue !== "" ? Number(inputValue) : "";
-            changeDiceObj("num", newValue);
-          }} 
-        />
-        {/* 骰子面数 */}
-        <Select 
-          value={diceObj.side} 
-          onChange={(e) => changeDiceObj("side", e.target.value)}
-        >
-          {
-            diceSideOptions.map(e => 
-              <option value={e.value} key={e.value}>{e.label}</option>
-            )
-          }
-        </Select>
-        <Button onClick={rollDice}>Roll</Button>
-      </div>
-      
-
-      <Text 
-        mt="30px"
-        fontSize={30} 
-        fontWeight={600} 
-        fontFamily="Times New Roman" 
-        textShadow="1px 2px #fff"
-      >DICE ROLL LOG</Text>
-      <List maxH="230px" overflow="hidden" overflowY="auto" bg="#fff" border="1px" borderRadius="10px" borderColor="#a52714" w="80%" margin="0 auto" padding="0 10px" >
-        {
-          rollLog.map((log, index) =>           
-            <li key={index} style={{ borderBottom: "1px solid #000" }}>
-              <Text fontStyle="italic">{log.time}</Text>
-              <Text>Rolling line: <strong>{log.rollLine}</strong></Text>
-              <Text>{log.total}</Text>
-              <Text>TOTAL: <span style={{color:"#a52714"}}>{log.totalNum}</span></Text>
-            </li>
-          )
         }
-      </List>
+        {/* 掷骰子 */}
+        <Button 
+          className="btn-roll"
+          h="30px"
+          padding="0 20px"
+          margin="10px auto 0"
+          bg="#a52714" 
+          border="1px solid #000" 
+          borderRadius="40px"
+          boxShadow="1px 1px 5px #000c"
+          fontSize="20px"
+          color="#fff" 
+          fontFamily="Times New Roman" 
+          textShadow="1px 1px #000" 
+          onClick={rollDice}
+        >Roll</Button>
+      </Box>
+
+      {/* 骰子日志打印 */}
+      {
+        rollLog.length !== 0 &&
+        <>
+          <Text 
+            mt="30px"
+            fontSize={30} 
+            fontWeight={600} 
+            fontFamily="Times New Roman" 
+            textShadow="1px 2px #fff"
+          >DICE ROLL LOG</Text>
+          <List className="List" maxH="230px" overflow="hidden" overflowY="auto" bg="#fff" border="1px" borderRadius="10px" borderColor="#a52714" w="80%" margin="0 auto" padding="10px" >
+              {
+                rollLog.map((log, index) =>           
+                  <li key={index} style={{ borderBottom: "1px solid #000" }}>
+                    <Text fontStyle="italic">{log.time}</Text>
+                    <Text>Rolling line: <strong>{log.rollLine}</strong></Text>
+                    <Text>{log.total}</Text>
+                    <Text>TOTAL: <span style={{color:"#a52714"}}>{log.totalNum}</span></Text>
+                  </li>
+                )
+              }
+          </List>
+        </>
+      }
     </div>
   );
 }
